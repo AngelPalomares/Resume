f21d09d7ce88397aa51266038e13e007cff7514f
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2024-06-10 17:24:32 -0500

    Update Click.js

--- Click.js
+++ Click.js
@@ -4,6 +4,24 @@ document.addEventListener('DOMContentLoaded', function() {
     var span = document.getElementsByClassName("close")[0];
     var iframe = document.getElementById("webpageFrame");
 
+    // Function to open the modal
+    function openModal(url) {
+        iframe.src = url;
+        modal.classList.add("opening");
+        modal.classList.remove("closing");
+        modal.style.display = "block";
+    }
+
+    // Function to close the modal
+    function closeModal() {
+        modal.classList.remove("opening");
+        modal.classList.add("closing");
+        setTimeout(function() {
+            modal.style.display = "none";
+            iframe.src = ""; // Clear the iframe src to stop loading the page
+        }, 500); // Match this to the transition duration
+    }
+
     // When the user clicks any button, open the modal and load the webpage
     buttons.forEach(function(button) {
         button.addEventListener('click', function(event) {
@@ -13,23 +31,20 @@ document.addEventListener('DOMContentLoaded', function() {
             if (url.includes("github.com") || url.includes("linkedin.com")) {
                 window.open(url, '_blank'); // Open in a new tab
             } else {
-                iframe.src = url;
-                modal.style.display = "block";
+                openModal(url); // Open the modal with the URL
             }
         });
     });
 
     // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
-        modal.style.display = "none";
-        iframe.src = ""; // Clear the iframe src to stop loading the page
+        closeModal();
     }
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
-            modal.style.display = "none";
-            iframe.src = ""; // Clear the iframe src to stop loading the page
+            closeModal();
         }
     }
 });
